751fb5f5eb5a2655a0ab07c378b022a61d79f357
Wouter van Verre <woutervanverre@gmail.com>
2021-12-30 22:09:57 +0000

    Clean up the axi-gpio example
    Signed-off-by: Wouter van Verre <woutervanverre@gmail.com>

--- axi-gpio/mk_proj.tcl
+++ axi-gpio/mk_proj.tcl
@@ -1,6 +1,6 @@
 set projectname "axi-gpio"
-set outputdir "./$projectname"
 set part xc7z010-1clg400
+set outputdir "./$projectname"
 
 # Create output directory and clear contents
 file mkdir $outputdir
@@ -17,14 +17,8 @@ create_project -part $part $projectname $outputdir
 
 source ./../src/bd/axi_gpio.tcl
 
-make_wrapper -files [get_files /home/wouter/git/red-pitaya-tutorials/axi-gpio/vivado/axi-gpio/axi-gpio.srcs/sources_1/bd/axi_gpio/axi_gpio.bd] -top
-add_files -norecurse /home/wouter/git/red-pitaya-tutorials/axi-gpio/vivado/axi-gpio/axi-gpio.srcs/sources_1/bd/axi_gpio/hdl/axi_gpio_wrapper.v
+make_wrapper -files [get_files $bdpath/$bdname.bd] -top
+add_files -norecurse $bdpath/hdl/axi_gpio_wrapper.v
 
 read_xdc ../src/xdc/constraints.xdc
 
-set_property top "${bd_name}_wrapper" [current_fileset]
-update_compile_order -fileset sources_1
-
-
-#Run implementation and generate bitstream
-set_property STEPS.PHYS_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
