1ff3da0ecdd14717cc85e4e0af275f3d61433d0a
Wouter van Verre <woutervanverre@gmail.com>
2022-02-11 10:21:58 +0000

    Clean up the blink-led and axi-gpio block design scripts
    Signed-off-by: Wouter van Verre <woutervanverre@gmail.com>

--- axi-gpio/src/bd/axi_gpio.tcl
+++ axi-gpio/src/bd/axi_gpio.tcl
@@ -1,25 +1,38 @@
 set bdname "axi_gpio"
 create_bd_design $bdname
 
-set processing_system [ create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 processing_system7 ]
-apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {make_external "FIXED_IO, DDR" Master "Disable" Slave "Disable" } $processing_system
+set processing_system [
+        create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 processing_system7
+    ]
 
-set_property -dict [list CONFIG.PCW_FPGA0_PERIPHERAL_FREQMHZ {125}] [get_bd_cells processing_system7]
+apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {
+        make_external "FIXED_IO, DDR" 
+        Master "Disable" 
+        Slave "Disable"
+    } $processing_system
+
+set_property -dict [list \
+        CONFIG.PCW_FPGA0_PERIPHERAL_FREQMHZ {125}
+    ] $processing_system
 
 set axi_gpio [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio:2.0 axi_gpio ]
 set_property -dict [list CONFIG.C_GPIO_WIDTH {8} CONFIG.C_ALL_OUTPUTS {1}] $axi_gpio
 
-apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { \
-    Clk_master {/processing_system7/FCLK_CLK0 (125 MHz)} \
-    Clk_slave {/processing_system7/FCLK_CLK0 (125 MHz)} \
-    Clk_xbar {/processing_system7/FCLK_CLK0 (125 MHz)} \
-    Master {/processing_system7/M_AXI_GP0} \
-    Slave {/axi_gpio/S_AXI} \
-    intc_ip {New AXI Interconnect} \
-    master_apm {0}
-}  [get_bd_intf_pins axi_gpio/S_AXI]
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {
+        Clk_master {/processing_system7/FCLK_CLK0 (125 MHz)}
+        Clk_slave {/processing_system7/FCLK_CLK0 (125 MHz)}
+        Clk_xbar {/processing_system7/FCLK_CLK0 (125 MHz)}
+        Master {/processing_system7/M_AXI_GP0}
+        Slave {/axi_gpio/S_AXI}
+        intc_ip {New AXI Interconnect}
+        master_apm {0}
+    }  [get_bd_intf_pins axi_gpio/S_AXI]
 
-apply_bd_automation -rule xilinx.com:bd_rule:board -config { Manual_Source {Auto}}  [get_bd_intf_pins axi_gpio/GPIO]
+apply_bd_automation -rule xilinx.com:bd_rule:board \
+    -config {
+        Manual_Source {Auto}
+    }  [get_bd_intf_pins axi_gpio/GPIO]
+
 set_property NAME leds [get_bd_intf_ports /gpio_rtl_0]
 
 set_property offset 0x41000000 [get_bd_addr_segs {processing_system7/Data/SEG_axi_gpio_Reg}]
--- blink-led/src/bd/blinky.tcl
+++ blink-led/src/bd/blinky.tcl
@@ -1,28 +1,28 @@
 set bdname "blinky"
 create_bd_design $bdname
 
-set processing_system [ \
-    create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 processing_system7 \
+set processing_system [
+    create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 processing_system7
 ]
 
-apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config { \
-    make_external "FIXED_IO, DDR" \
-    Master "Disable" \
-    Slave "Disable" \
+apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {
+    make_external "FIXED_IO, DDR"
+    Master "Disable"
+    Slave "Disable"
 } $processing_system
 
 set_property -dict [list \
     CONFIG.PCW_FPGA0_PERIPHERAL_FREQMHZ {125} \
-    CONFIG.PCW_USE_M_AXI_GP0 {0} \
+    CONFIG.PCW_USE_M_AXI_GP0 {0}
 ] $processing_system
 
 
-set binary_counter [ \
-    create_bd_cell -type ip -vlnv xilinx.com:ip:c_counter_binary:12.0 c_counter_binary \
+set binary_counter [
+    create_bd_cell -type ip -vlnv xilinx.com:ip:c_counter_binary:12.0 c_counter_binary
 ]
 
-set slice_counter [ \
-    create_bd_cell -type ip -vlnv xilinx.com:ip:xlslice:1.0 xlslice_counter \
+set slice_counter [
+    create_bd_cell -type ip -vlnv xilinx.com:ip:xlslice:1.0 xlslice_counter
 ]
 
 set_property -dict [list CONFIG.Output_Width {27}] $binary_counter
